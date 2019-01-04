4c446fa59efbd947c50442fdc8eb0a8e711bbf17
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2019-01-04 09:26:38 +0200

    Rental listing Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { RentalModule} from './rental/rental.module';
+
+
+const routes: Routes = [
+  {path: '', component: RentalComponent}
+]
 
 @NgModule({
   declarations: [
@@ -11,8 +19,10 @@ import { HeaderComponent } from './common/header/header.component';
     HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
