2e0441ae6a2355bba0b790d411c94acd0a8541a3
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2019-01-07 17:05:49 +0200

    Add register, loggin and auth middleware routes on server

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'; 
 
 import { Rental } from './rental.model';
